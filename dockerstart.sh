8a4e728b35a6b4e3d95a4b34e653737bb21a3520
Sam Billerbeck <billerbeck@ou.edu>
2023-08-30 17:41:16 -0500

    Created scripts

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 xhost +si:localuser:root
-
 mkdir -p ~/rosdocker/compose
+mkdir -p ~/rosdocker/data
 cd ~/rosdocker/compose
-git clone https://github.com/oucompsci/docker.git
-sudo docker-compose up
-
-sudo docker compose exec rosbash bash
+docker-compose up
