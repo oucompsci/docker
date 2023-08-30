a202d652c88faf41eeef2013f4759905d13782dd
Sam Billerbeck <billerbeck@ou.edu>
2023-08-30 17:49:24 -0500

    Changed start and added install script

---
+++ dockerinstall.sh
@@ -0,0 +1,4 @@
+currdir=$(pwd)
+mkdir -p ~/rosdocker/compose
+mv $currdir/dockerstart.sh ~/rosdocker/compose
+mv $currdir/compose.yaml ~/rosdocker/compose
--- dockerstart.sh
+++ dockerstart.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 xhost +si:localuser:root
 mkdir -p ~/rosdocker/compose
-mkdir -p ~/rosdocker/data
 cd ~/rosdocker/compose
 docker-compose up
