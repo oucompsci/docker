9bd80265d40c4f2304a14d65a8347a3f1c968127
Sam Billerbeck <billerbeck@ou.edu>
2023-09-05 13:25:20 -0500

    Updated readme, fixed install script

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -3,12 +3,11 @@
 if ! grep -q "xhost +si:localuser:root" ~/.bashrc; then
   echo "xhost +si:localuser:root" >> ~/.bashrc
 fi
-
+xhost +si:localuser:root
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
 currdir=$(pwd)
 mkdir -p ~/rosdocker/compose
-mv $currdir/dockerstart.sh ~/rosdocker/compose
 mv $currdir/compose.yaml ~/rosdocker/compose
