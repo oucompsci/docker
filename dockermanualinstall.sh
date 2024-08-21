4e591af15b6a3c9707e8cc42d33b487fb698a34c
oucompsci <86008076+oucompsci@users.noreply.github.com>
2024-08-21 10:22:45 -0500

    Update dockermanualinstall.sh

--- dockermanualinstall.sh
+++ dockermanualinstall.sh
@@ -3,7 +3,12 @@
 if ! grep -q "xhost +si:localuser:root" ~/.bashrc; then
   echo "xhost +si:localuser:root" >> ~/.bashrc
 fi
+
+if ! grep -q "export DOCKER_USER=$(id -u)" ~/.bashrc; then
+  echo "export DOCKER_USER=$(id -u)" >> ~/.bashrc
+fi
 xhost +si:localuser:root
+export DOCKER_USER=$(id -u)
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
